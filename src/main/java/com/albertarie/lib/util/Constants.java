a104da2727b7da69e4268f7c8a30dce9fc775f07
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2024-02-17 13:16:40 +0000

    Update API_BASE constant in Constants.java
    A forward slash has been added at the end of the API_BASE constant in Constants.java. This is done to ensure that the URL for the API calls is always consistent and properly formatted, which improves the reliability of the requests made using this constant.

--- src/main/java/com/albertarie/lib/util/Constants.java
+++ src/main/java/com/albertarie/lib/util/Constants.java
@@ -1,7 +1,7 @@
 package com.albertarie.lib.util;
 
 public class Constants {
-    public static final String API_BASE = "https://api.the-odds-api.com/v4/sports";
+    public static final String API_BASE = "https://api.the-odds-api.com/v4/sports/";
 
     public static final String ODDS_ENDPOINT_FMT = "%s/odds";
     public static final String SCORES_ENDPOINT_FMT = "%s/scores";
